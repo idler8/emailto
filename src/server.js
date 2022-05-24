45132837eaba00b3097eb1c680fe09562927cf53
闲人 <code@idler8.com>
2022-05-24 17:10:36 +0800

    feat: 更简单的健康检查

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const { FROM, DELAY, PORT, AUTO } = require('./config.js');
-const dns = require('./libs/dns.js');
 const mailto = require('./libs/mailto.js');
 const { generate } = require('./libs/dkim.js');
 
@@ -8,13 +7,7 @@ const app = express();
 app.use(express.json({ strict: false }));
 app.use(express.urlencoded({ extended: true }));
 app.get('/health', async function(req, res) {
-  if (!app.locals.dkim) {
-    app.locals.STATE = false;
-    res.json('EMPTY');
-  } else {
-    app.locals.STATE = await dns(app.locals.dkim);
-    res.json('OK');
-  }
+  res.json('OK');
 });
 app.post('/dkim/ali', async function(req, res) {
   if (app.locals.LOCKED) {
@@ -28,7 +21,7 @@ app.post('/dkim/ali', async function(req, res) {
 });
 const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/;
 app.post('/mailto', function(req, res) {
-  if (!app.locals.STATE) return res.json({ '_error': { 'message': '系统准备中' }});
+  if (!app.locals.dkim) return res.json({ '_error': { 'message': '系统准备中' }});
   const { to, subject, text, html, attachments } = req.body;
   if (!regEmail.test(to)) return res.json({ '_error': { 'message': '错误的目标地址' }});
   if (!subject) return res.json({ '_error': { 'message': '缺少标题' }});
